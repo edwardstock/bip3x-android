a75a70a8fb3c92fef5176634f6a1cf7703ecf4b1
Eduard Maximovich <edward.vstock@gmail.com>
2019-12-12 22:22:57 +0300

    fix for ci script

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y gcc g++ gdb make curl wget
+apt-get install -y git gcc g++ gdb make curl wget
 
 if [ ! -d "/tmp/pkgs" ]; then
   mkdir -p /tmp/pkgs
